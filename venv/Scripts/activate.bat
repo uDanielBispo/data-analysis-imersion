08d71362d1d9542dbee365dc87508d51c72026cc
Daniel Bispo <danielbispo3015@hotmail.com>
2025-05-03 09:57:31 -0300

    Fixing git ignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\danie\Downloads\Arquivos_Fora_do_OneDrive\alura\imsersao-analises\descriptive-analysis\venv
+set VIRTUAL_ENV=C:\Users\danie\Downloads\Arquivos_Fora_do_OneDrive\alura\imsersao-analises\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
